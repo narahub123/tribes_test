045d80750178fbb3b19d9a53a3e55ff2d6b5392b
narahub123 <parknara0213@gmail.com>
2025-10-30 15:10:56 +0900

    Feat: MissionSection 완성

--- src/ui/comu/sections/MissionSection.tsx
+++ src/ui/comu/sections/MissionSection.tsx
@@ -1,9 +1,16 @@
+import { MainComu } from "../../../data";
 import { ComuSectionHeader } from "../ComuSectionHeader";
 
 export const MissionSection = () => {
+  const { title, paragraphs } = MainComu.mission;
   return (
     <section>
-      <ComuSectionHeader title={"미션"} className="" />
+      <ComuSectionHeader title={title} className="" />
+      <div className="p-4 space-y-4">
+        {paragraphs.map((p) => (
+          <p>{p}</p>
+        ))}
+      </div>
     </section>
   );
 };
