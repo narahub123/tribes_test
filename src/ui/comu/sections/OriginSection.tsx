4207b0f7761a7a62507afbb9f3b0f0a9fb664923
narahub123 <parknara0213@gmail.com>
2025-10-30 15:06:06 +0900

    Feat: OriginSection 완성

--- src/ui/comu/sections/OriginSection.tsx
+++ src/ui/comu/sections/OriginSection.tsx
@@ -1,9 +1,14 @@
+import { MainComu } from "../../../data";
 import { ComuSectionHeader } from "../ComuSectionHeader";
 
 export const OriginSection = () => {
+  const { title, text } = MainComu.origin;
   return (
     <section>
-      <ComuSectionHeader title={"탄생 스토리"} className="" />
+      <ComuSectionHeader title={title} className="" />
+      <div className="p-4">
+        <p>{text}</p>
+      </div>
     </section>
   );
 };
