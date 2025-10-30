b459aa9013ba2b6af1c7836833fd9ea4541be479
narahub123 <parknara0213@gmail.com>
2025-10-30 15:08:22 +0900

    Feat: VisionSection 완성

--- src/ui/comu/sections/VisionSection.tsx
+++ src/ui/comu/sections/VisionSection.tsx
@@ -1,9 +1,14 @@
+import { MainComu } from "../../../data";
 import { ComuSectionHeader } from "../ComuSectionHeader";
 
 export const VisionSection = () => {
+  const { title, text } = MainComu.vision;
   return (
     <section>
-      <ComuSectionHeader title={"비전"} className="" />
+      <ComuSectionHeader title={title} />
+      <div className="p-4">
+        <p>{text}</p>
+      </div>
     </section>
   );
 };
