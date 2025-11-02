353410d382d7c8f452ad8748ebf2057abfb47cd9
narahub123 <parknara0213@gmail.com>
2025-11-02 22:49:30 +0900

    Feat: 스크롤 문제 해결

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,7 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Footer, Header } from "../layouts";
+import { useEffect } from "react";
 
 export const MainLayout = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  
   return (
     <div className="max-w-[1200px] inset-0 m-auto shadow-2xl">
       <Header />
